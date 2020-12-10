5def8ae0ff6d582f56a7644a20d7b95d9c04e8f9
hongxinzz <510348414@qq.com>
2020-12-10 10:03:37 +0800

    :lipstick: update style

--- src/hooks/useMouse.ts
+++ src/hooks/useMouse.ts
@@ -3,27 +3,27 @@
  * @LastEditors: xinghe
  * @Date: 2020-12-10 09:23:33
  * @FilePath: /vue3-project/src/hooks/useMouse.ts
- * @LastEditTime: 2020-12-10 09:28:52
+ * @LastEditTime: 2020-12-10 10:02:01
  */
-import { ref, onMounted,onUnmounted } from 'vue'
-const useMouse = ()=> {
-    const x = ref(0),y = ref(0);
+import { ref, onMounted, onUnmounted } from 'vue'
+const useMouse = () => {
+    const x = ref(0), y = ref(0);
 
-    const updateMouse = (e: MouseEvent) =>{
-        const {pageX,pageY} = e;
+    const updateMouse = (e: MouseEvent) => {
+        const { pageX, pageY } = e;
         x.value = pageX;
         y.value = pageY;
     }
 
-    onMounted(()=>{
-        document.addEventListener('click',updateMouse)
+    onMounted(() => {
+        document.addEventListener('click', updateMouse)
     });
 
-    onUnmounted(()=>{
-        document.removeEventListener('click',updateMouse)
+    onUnmounted(() => {
+        document.removeEventListener('click', updateMouse)
     })
 
-    return {x,y}
+    return { x, y }
 }
 
 export default useMouse;
