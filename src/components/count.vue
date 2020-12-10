21f4e9acbd0d8c03e25c209a422a26e6cf5bc500
hongxinzz <510348414@qq.com>
2020-12-10 09:12:00 +0800

    :sparkles: onMounted, onUpdated, onRenderTriggered, watch

--- src/components/count.vue
+++ src/components/count.vue
@@ -3,7 +3,7 @@
  * @LastEditors: xinghe
  * @Date: 2020-12-09 22:56:58
  * @FilePath: /vue3-project/src/components/count.vue
- * @LastEditTime: 2020-12-09 23:10:47
+ * @LastEditTime: 2020-12-10 09:11:25
 -->
 <template>
   <div>
@@ -13,23 +13,40 @@
     <div>
       {{double}}
     </div>
+    <div v-for="item in numbers" :key="item">
+        {{item}}
+    </div>
+    <div>
+        {{person.name}}
+    </div>
     <div @click="increase"> 👍 + 1</div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, ref, reactive, toRefs} from 'vue';
+import { computed, ref, reactive, toRefs, onMounted, onUpdated, onRenderTriggered, watch} from 'vue';
 interface DataProps{
     count: number;
     double: number;
     increase: () => void;
+    numbers: number[];
+    person: {name?: string};
 }
 export default {
   name:'app',
   setup(){
+      onMounted(()=>{
+          console.log('mounted')
+      })
+    //   onUpdated(() => {
+    //       console.log('updated')
+    //   })
+    //   onRenderTriggered(e => {
+    //       console.log(e,'onRenderTriggered')
+    //   })
     //   const count = ref(0);
 
-    //   const double = computed(()=>{
+    //   const double = computed(() => {
     //     return count.value * 2
     //   });
     //   const increase = () => {
@@ -38,9 +55,22 @@ export default {
     const data: DataProps = reactive({
         count: 0,
         increase: () => { data.count++ },
-        double: computed(() => data.count * 2)
+        double: computed(() => data.count * 2),
+        numbers:[0,1,2],
+        person:{}
     })
 
+    watch(() => data.count, (newVal,oldVal) => {
+        console.log(newVal,oldVal)
+        if(newVal % 2 === 0){
+            document.title = 'hello:' + newVal;
+        }else{
+            document.title = 'hello'
+        }
+    })
+
+    data.numbers[0] = 5;
+    data.person.name = "test"
     const refData = toRefs(data)
     // refData.count
 
